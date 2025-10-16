3abaa6fc6d9d50c272a2a5d4772dc183f24ab871
Shantanu Waikar <shantanuwaikar10@gmail.com>
2025-10-16 21:49:17 +0530

    updated

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,18 +1,39 @@
+import { button } from "@material-tailwind/react";
+import { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 const Details = () => {
+  const [activeTab, setActiveTab] = useState("data_analysis");
+
+  const tabs = [
+    { id: "data_analysis", label: "Data Analysis" },
+    { id: "graphical_display", label: "Graphical Display" },
+    { id: "query_informations", label: "Query Information" },
+    { id: "downloads", label: "Downloads" },
+  ];
+
   return (
     <div className="h-60 w-full overflow-auto bg-[#171c22] p-4">
-      <div className="bg-[#1c232b] min-h-full p-2 rounded-xl">
-        <Tabs>
-          <TabList className="flex gap-8 mb-2">
-            <Tab className="text-white cursor-pointer focus:text-blue-400">Data Analysis</Tab>
-            <Tab className="text-white cursor-pointer focus:text-blue-400">Graphical Display</Tab>
-            <Tab className="text-white cursor-pointer focus:text-blue-400">Query Information</Tab>
-            <Tab className="text-white cursor-pointer focus:text-blue-400">Downloads</Tab>
+      <div className="bg-[#1c232b] min-h-full pl-2 rounded-xl">
+        <Tabs className="">
+          <TabList className="flex gap-10">
+            {tabs.map((tab) => (
+              <Tab
+                key={tab.id}
+                onClick={() => setActiveTab(tab.id)}
+                className={` py-2 text-center font-medium transition-all duration-300
+              ${
+                activeTab === tab.id
+                  ? "text-blue-400 border-b-2 border-blue-400 cursor-pointer"
+                  : "text-white hover:text-blue-500 cursor-pointer"
+              }`}
+              >
+                {tab.label}
+              </Tab>
+            ))}
           </TabList>
 
-          <hr className="border-white"/>
+          <hr className="border-white" />
 
           <TabPanel>
             <h2 className="text-white">Content 1</h2>
